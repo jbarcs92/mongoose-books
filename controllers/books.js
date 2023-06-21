a3040d14566df0f24635e0d25df0d714fbd7bc44
jbarcs92 <jbarcl92@gmail.com>
2023-06-21 12:01:40 -0400

    Final commit

--- controllers/books.js
+++ controllers/books.js
@@ -11,30 +11,24 @@ module.exports = {
 };
 
 async function index(req,res) {
+  try {
     const books = await Book.find({});
     res.render('books/index', { books });
+  } catch (err) {
+    console.log(err);
+    res.render('books/index', { errorMsg: err.message });
+  }
 }
 
-// function show(req, res) {
-//     res.render('books/show', {
-//         book: Book.getOne(req.params.title)
-//     });
-// }  
-
-// async function show(req, res){
-//     const books = await Book.find({});
-//     res.render('books/show', { books })
-// }
-
 async function show(req, res) {
-    const book = await Book.findOne({ title: req.params.title });
-    res.render('books/show', { book });
+  try {
+   const book = await Book.findOne({ title: req.params.title });
+   res.render('books/show', { book });
+  } catch (err) {
+    console.log(err);
+    res.render('books/show', { errorMsg: err.message });
   }
-
-// function show(req, res) {
-//     const book = Book.getOne(req.params.title);
-//     res.render('book', { book });
-//   }
+}
 
 function newBook(req,res) {
     res.render('books/new', { errorMsg: '' });
@@ -54,17 +48,20 @@ async function create(req, res) {
 }
 
  async function edit (req, res) {
-    const book = await Book.findOne({ title: req.params.title });
-    res.render('books/edit', {
-        title: 'Edit To-Do'
-    });
-}
+    try {
+      const book = await Book.findOne({ title: req.params.title });
+      res.render('books/edit', { title: 'Edit To-Do', book });
+    } catch (err) {
+      console.log(err);
+      res.render('books/edit', { errorMsg: err.message });
+    }
+  }
 
 async function update(req, res) {
     try {
-      await Book.findOneAndUpdate({ title: req.params.title })
-      res.redirect(`/books/${req.params.title}`);
-    }  catch (err) {
+      await Book.findOneAndUpdate({title: req.params.title}, req.body);
+      res.redirect('/books');
+    } catch (err) {
       res.render(`/books/${req.params.title}/edit`, { errorMsg: err.message });
     }
   }
--- routes/books.js
+++ routes/books.js
@@ -5,10 +5,11 @@ const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.index);
 router.get('/new', booksCtrl.new);
+router.get('/:title/edit', booksCtrl.edit);
 router.get('/:title', booksCtrl.show);
 router.post('/', booksCtrl.create);
-router.delete('/:title', booksCtrl.delete);
-router.get('/:title/edit', booksCtrl.edit);
 router.put('/:title', booksCtrl.update);
+router.delete('/:title', booksCtrl.delete);
+
 
 module.exports = router;
